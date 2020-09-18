e7a69a08926387fd4439a6443fe5fbc15645add6
panda421 <weining421@163.com>
2020-09-18 17:11:07 +0800

    515 go go go!

---
+++ src/main/java/cn/edu/zjut/csma/label/LabelApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.zjut.csma.label;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/zjut/csma/label/LabelApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.zjut.csma.label;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
