a696e64545c5a6f21a54b87a833581ba17fa8cf8
jtj <1053552191@qq.com>
2020-09-18 20:15:49 +0800

    添加一个controller样例

---
+++ src/main/java/cn/edu/zjut/csma/label/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package cn.edu.zjut.csma.label.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/welcome")
+@CrossOrigin
+public class WelcomeController {
+    //添加章节
+    @GetMapping("")
+    public String addChapter() {
+       return "welcome";
+    }
+}
