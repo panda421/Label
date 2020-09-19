5a2bea38b670bec2d2b05a9b4c793168079289f1
jtj <1053552191@qq.com>
2020-09-19 15:54:43 +0800

    添加一个controller样例

--- src/main/java/cn/edu/zjut/csma/label/LabelApplication.java
+++ src/main/java/cn/edu/zjut/csma/label/LabelApplication.java
@@ -1,9 +1,11 @@
 package cn.edu.zjut.csma.label;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.edu.zjut.csma.label.mapper")
 public class LabelApplication {
 
     public static void main(String[] args) {
--- src/main/java/cn/edu/zjut/csma/label/service/impl/NewsInfoServiceImpl.java
+++ src/main/java/cn/edu/zjut/csma/label/service/impl/NewsInfoServiceImpl.java
@@ -3,8 +3,8 @@ package cn.edu.zjut.csma.label.service.impl;
 import cn.edu.zjut.csma.label.entity.NewsInfo;
 import cn.edu.zjut.csma.label.mapper.NewsInfoMapper;
 import cn.edu.zjut.csma.label.service.NewsInfoService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 
@@ -13,8 +13,10 @@ public class NewsInfoServiceImpl extends ServiceImpl<NewsInfoMapper, NewsInfo> i
 
     //根据课程id查询课程基本信息
     @Override
-    public NewsInfo getNewsInfo(String newsId) {
-        NewsInfo newsInfo = baseMapper.selectById(newsId);
+    public NewsInfo getNewsInfo() {
+        QueryWrapper wrapper=new QueryWrapper();
+        wrapper.eq("news_id","324355");
+        NewsInfo newsInfo=baseMapper.selectOne(wrapper);
         System.out.println(newsInfo);
         return newsInfo;
     }
