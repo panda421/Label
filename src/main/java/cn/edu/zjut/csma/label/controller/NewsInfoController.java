5a8bfeaa8298d45d6fc5adb50c0029e6d5d76cd3
jtj <1053552191@qq.com>
2020-09-19 15:30:16 +0800

    添加一个controller样例

---
+++ src/main/java/cn/edu/zjut/csma/label/controller/NewsInfoController.java
@@ -0,0 +1,24 @@
+package cn.edu.zjut.csma.label.controller;
+
+
+import cn.edu.zjut.csma.label.entity.NewsInfo;
+import cn.edu.zjut.csma.label.service.NewsInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/labelservice/newsInfo")
+@CrossOrigin
+public class NewsInfoController {
+    @Autowired
+    private NewsInfoService newsInfoService;
+
+    //添加章节
+    @GetMapping("getNewsInfo")
+    public String getNewsInfo(String newsId){
+        NewsInfo newsInfo=newsInfoService.getNewsInfo(newsId);
+        return newsInfo.toString();
+    }
+}
